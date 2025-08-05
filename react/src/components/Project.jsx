34c172d27c6f21e886341782dcd759c346c1a0f1
Kapil Raut <kapilraut026@gmail.com>
2025-08-05 17:49:20 +0545

    Update Project.jsx

--- react/src/components/Project.jsx
+++ react/src/components/Project.jsx
@@ -24,7 +24,7 @@ import {motion} from 'framer-motion'
  initial={{ opacity: 0, x:100}}
  transition={{duration: 2.5 }}
  className='w-full max-w-xl lg:w-3/4 max-sm:text-center'>
-<h6 className='mb-2 font-semibold'>{project.title}</h6>
+<h6 className='mb-2 font-semibold'><a href="https://kapilraut.com.np/">{project.title}</a></h6>
       <p className='mb-4 text-neutral-400'>{project.description}</p> 
        {project.technologies.map((tech, index) =>(
         <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-2 text-sm font-medium text-purple-900'>
